8e88ff3ded4aac1a842f93ddcbf16250a84425e4
Robert Simionescu <robsim378@gmail.com>
2023-11-27 17:49:35 -0500

    Trying another thing

--- ros2_ws/launch/test_launch.py
+++ ros2_ws/launch/test_launch.py
@@ -14,8 +14,11 @@ def generate_launch_description():
             ),
         launch.actions.DeclareLaunchArgument(
             name='objects_definition_file',
+#            default_value=get_package_share_directory(
+                # 'carla_spawn_objects') + '/config/objects.json'
             default_value=get_package_share_directory(
-                'carla_spawn_objects') + '/config/objects.json'
+                'lane_nodes_py') + '/config/objects.json'
+                )
             ),
         launch.actions.DeclareLaunchArgument(
             name='spawn_point_ego_vehicle',
