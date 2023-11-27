f713678c4e9120fac37d5b4f8dc794b20c4b4d8d
Robert Simionescu <robsim378@gmail.com>
2023-11-27 17:02:09 -0500

    Updated launch file

--- ros2_ws/launch/test_launch.py
+++ ros2_ws/launch/test_launch.py
@@ -11,38 +11,75 @@ def generate_launch_description():
         launch.actions.DeclareLaunchArgument(
             name='role_name',
             default_value='ego_vehicle'
-        ),
+            ),
+        launch.actions.DeclareLaunchArgument(
+            name='objects_definition_file',
+            default_value='../carla_config/objects.json'
+            ),
+        launch.actions.DeclareLaunchArgument(
+            name='spawn_point_ego_vehicle',
+            default_value='None'
+            ),
+        launch.actions.DeclareLaunchArgument(
+            name='spawn_sensors_only',
+            default_value='False'
+            ),
+        launch.actions.DeclareLaunchArgument(
+            name='control_id',
+            default_value='control'
+            ),
         Node(
             package='lane_nodes_py',
             namespace='test',
             executable='camera_controller_node',
             name='camera_controller'
-        ),
+            ),
         Node(
             package='lane_nodes_py',
             namespace='test',
             executable='detection_node',
             name='detection'
-        ),
+            ),
         Node(
             package='lane_nodes_py',
             namespace='test',
             executable='keeping_node',
             name='keeping'
-        ),
+            ),
         Node(
             package='lane_nodes_cpp',
             namespace='test',
             executable='movement_controller',
             name='movement_controller'
-        ),
+            ),
         launch.actions.IncludeLaunchDescription(
             launch.launch_description_sources.PythonLaunchDescriptionSource(
                 os.path.join(get_package_share_directory(
                     'carla_ackermann_control'), 'carla_ackermann_control.launch.py')
-            ),
+                ),
             launch_arguments={
                 'role_name': launch.substitutions.LaunchConfiguration('role_name')
+                }.items()
+            ),
+        launch.actions.IncludeLaunchDescription(
+            launch.launch_description_sources.PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory(
+                    'carla_spawn_objects'), 'carla_spawn_objects.launch.py')
+                ),
+            launch_arguments={
+                'objects_definition_file': launch.substitutions.LaunchConfiguration('objects_definition_file'),
+                'spawn_point_ego_vehicle': launch.substitutions.LaunchConfiguration('spawn_point_ego_vehicle'),
+                'spawn_sensors_only': launch.substitutions.LaunchConfiguration('spawn_sensors_only')
+                }.items()
+            ),
+        launch.actions.IncludeLaunchDescription(
+            launch.launch_description_sources.PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory(
+                    'carla_spawn_objects'), 'set_initial_pose.launch.py')
+            ),
+            launch_arguments={
+                'role_name': launch.substitutions.LaunchConfiguration('role_name'),
+                'control_id': launch.substitutions.LaunchConfiguration('control_id')
             }.items()
         )
     ])
--- ros2_ws/run.sh
+++ ros2_ws/run.sh
@@ -2,6 +2,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ros2 launch carla_ros_bridge carla_ros_bridge.launch.py synchronous_mode:=false town:=town04 register_all_sensors:=false &
-ros2 launch carla_spawn_objects carla_spawn_objects.launch.py objects_definition_file:=$SCRIPT_DIR/carla_config/objects.json
+#ros2 launch carla_spawn_objects carla_spawn_objects.launch.py objects_definition_file:=$SCRIPT_DIR/carla_config/objects.json
 ros2 launch ./launch/test_launch.py &
 /bin/bash
