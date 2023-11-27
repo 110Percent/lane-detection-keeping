fc9eba77218837bfb5504a9561d0bdb5d41ae226
Robert Simionescu <46873234+robsim378@users.noreply.github.com>
2023-11-27 18:40:46 -0500

    Custom vehicle (#2)
    * Added custom vehicle definition file at carla_config/objects.json
    
    * Added bash script to steamline launching
    
    * Updated launch file
    
    * Hopefully fixed launch file. Moved ego vehicle definition file.
    
    * Changed default ego vehicle definition file
    
    * Minor bugfix in camera_controller_node.py
    
    * Updated run script to add custom object definition file as launch parameter
    
    * trying something
    
    * Trying another thing
    
    * oops
    
    * Updated setup.py
    
    * my bad
    
    * Custom vehicle files working, next step is to add manual control back in
    
    * Added manual control to launch file
    
    * Attempt #2 at adding manual control
    
    * Removed the 90 degree roll from cameras that was being used to test that the object config file was correctly being used

--- ros2_ws/run.sh
+++ ros2_ws/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-ros2 launch carla_ros_bridge carla_ros_bridge_with_example_ego_vehicle.launch.py synchronous_mode:=false town:=town04 register_all_sensors:=false &
+ros2 launch carla_ros_bridge carla_ros_bridge.launch.py synchronous_mode:=false town:=town04 register_all_sensors:=false &
 ros2 launch ./launch/test_launch.py &
 /bin/bash
