cf3021c8de231e01816e3a819cca1456fa339a7b
Robert Simionescu <robsim378@gmail.com>
2023-11-27 13:33:34 -0500

    Added custom vehicle definition file at carla_config/objects.json

--- ros2_ws/run.sh
+++ ros2_ws/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-ros2 launch carla_ros_bridge carla_ros_bridge_with_example_ego_vehicle.launch.py synchronous_mode:=false town:=town04 register_all_sensors:=false &
+ros2 launch carla_ros_bridge carla_ros_bridge.launch.py synchronous_mode:=false town:=town04 register_all_sensors:=false &
+ros2 launch carla_spawn_objects carla_spawn_objects.launch.py objects_definition_file:=$SCRIPT_DIR/carla_config/objects.json
 ros2 launch ./launch/test_launch.py &
 /bin/bash
