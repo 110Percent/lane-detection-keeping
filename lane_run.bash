66fb22020c50214110613df6b0b46eee2b2e5c88
Robert Simionescu <robsim378@gmail.com>
2023-11-27 18:42:50 -0500

    Updated lane_run.bash to lengthen sleep, the new launch procedure seems to cause some issues if the ros docker container is started too quickly after the carla docker container

--- lane_run.bash
+++ lane_run.bash
@@ -17,7 +17,7 @@ echo ""
 xhost local:docker &
 docker run --name carla_docker -d --privileged --gpus=all --net=host -v /tmp/.X11-unix:/tmp/.X11-unix:rw carlasim/carla:0.9.13 /bin/bash ./CarlaUE4.sh -RenderOffScreen &
 
-sleep 3
+sleep 8
 
 docker run --name ros_docker -d --net=host --gpus=all -e DISPLAY=$DISPLAY -it lane &
 
