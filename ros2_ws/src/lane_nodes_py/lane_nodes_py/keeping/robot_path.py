a9fff3ba6a3520b385eccace3d78e32d76a4e731
dwarflord1 <saggysar@gmail.com>
2024-01-15 08:36:25 -0500

    forgor

--- ros2_ws/src/lane_nodes_py/lane_nodes_py/keeping/robot_path.py
+++ ros2_ws/src/lane_nodes_py/lane_nodes_py/keeping/robot_path.py
@@ -1,12 +1,18 @@
 from lane_location_interface import LaneData
 
+from ackerman_wrapper import AckermannWrapper
+
 
 def calculate_path(calculated_path: LaneData) -> list[tuple[int, int]]:
+    # TODO: Implement calculation of the lane path
+    print("Calculating path from lane data")
     return [(1, 1)]
 
 
 def polish_path(calculated_path: list[tuple[int, int]]) -> list[tuple[int, int]]:
-    return [(1, 1)]
+    # TODO: Implement the polishing of the calculated path
+    print("Polishing the calculated path")
+    return calculated_path
 
 
 class PathData:
@@ -16,15 +22,17 @@ class PathData:
     # Position of the car in the form of (x,y)
     position: tuple[int, int]
 
-    # Direction of the car in radians
+    # Direction of the car in radians, with zero representing "forward" across the x-axis
     car_direction: int
 
     def __init__(self, size):
         self.vehicle_size = size
         self.fresh = True
 
-    def update(self, data):
-        self.fresh = True
+    def update(self, last_message: AckermannWrapper):
+        # TODO: Implement the update of the mathematical model
+        print("Updating the mathematical model")
+        self.fresh = False
 
     def is_fresh(self):
         return self.fresh
