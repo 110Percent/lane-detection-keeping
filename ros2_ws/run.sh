251aee1f586fd45732df012b427ab65ba75133ce
Curtis <110percent@riseup.net>
2024-01-15 14:22:38 -0500

    Update main ROS launch filenames

--- ros2_ws/run.sh
+++ ros2_ws/run.sh
@@ -2,5 +2,5 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ros2 launch carla_ros_bridge carla_ros_bridge.launch.py synchronous_mode:=false town:=town04 register_all_sensors:=false &
-ros2 launch ./launch/test_launch.py &
+ros2 launch ./launch/launch_main.py &
 /bin/bash
