7e91317a18118a913399c33b7fbc4f1ad01d8452
dwarflord1 <saggysar@gmail.com>
2024-03-07 17:58:55 -0500

    UP YOURS WOKE MORALISTS, WELL SEE WHO CANCELS WHO

--- ros2_ws/src/lane_nodes_py/lane_nodes_py/evaluation_printer.py
+++ ros2_ws/src/lane_nodes_py/lane_nodes_py/evaluation_printer.py
@@ -4,84 +4,102 @@ from lane_nodes_py.evaluation_helper import quaternion_to_euler, dist, euler_to_
 import numpy as np
 
 
-def print_measurement_diagrams(vehicle_path, waypoints):
-    pass
+class DataAnalyzer():
+    lateral_error = []
 
+    heading_error = []
 
-def print_path_diagram(vehicle_path, waypoints):
-    vehicle_path_points = []
-    for p in vehicle_path:
-        vehicle_path_points += [(p.pose.pose.position.x, p.pose.pose.position.y)]
-    plt.plot(list(zip(*waypoints))[0], list(zip(*waypoints))[1])
-    plt.plot(list(zip(*vehicle_path_points))[0], list(zip(*vehicle_path_points))[1])
-    plt.show()
+    def __init__(self, vehicle_path, waypoints):
 
+        self.vehicle_path = vehicle_path
+        self.waypoints = waypoints
 
-def get_maximum_errors(vehicle_path, waypoints, logga):
-    # Get the closest two points to the cars position
-    max_distance = None
-    max_heading_error = None
-    for vehicle_point in vehicle_path:
-        closest = None
+        self.calculate_error_over_time()
 
-        current_location = (vehicle_point.pose.pose.position.x, vehicle_point.pose.pose.position.y,
-                            vehicle_point.pose.pose.position.z)
-        for i in range(len(waypoints)):
-            waypoint = waypoints[i]
-            if closest is None:
-                closest = i
-            elif (dist(waypoint, current_location) < dist(waypoints[closest], current_location) and
-                  abs(waypoint[2] - current_location[2]) < 5):
-                closest = i
-        front = None
-        back = None
-        if closest == 0:
-            front = np.array((waypoints[1][0], waypoints[1][1]))
-            back = np.array((waypoints[0][0], waypoints[0][1]))
-        elif closest == (len(waypoints) - 1):
-            front = np.array((waypoints[closest][0], waypoints[closest][1]))
-            back = np.array((waypoints[closest-1][0], waypoints[closest-1][1]))
-        else:
-            if dist(current_location, waypoints[closest - 1]) < dist(current_location, waypoints[closest + 1]):
-                front = np.array((waypoints[closest][0], waypoints[closest][1]))
-                back = np.array((waypoints[closest-1][0], waypoints[closest-1][1]))
+    def calculate_error_over_time(self):
+
+        for vehicle_point in self.vehicle_path:
+            closest = None
+
+            current_location = (vehicle_point.pose.pose.position.x, vehicle_point.pose.pose.position.y,
+                                vehicle_point.pose.pose.position.z)
+            for i in range(len(self.waypoints)):
+                waypoint = self.waypoints[i]
+                if closest is None:
+                    closest = i
+                elif (dist(waypoint, current_location) < dist(self.waypoints[closest], current_location) and
+                      abs(waypoint[2] - current_location[2]) < 5):
+                    closest = i
+            front = None
+            back = None
+            if closest == 0:
+                front = np.array((self.waypoints[1][0], self.waypoints[1][1]))
+                back = np.array((self.waypoints[0][0], self.waypoints[0][1]))
+            elif closest == (len(self.waypoints) - 1):
+                front = np.array((self.waypoints[closest][0], self.waypoints[closest][1]))
+                back = np.array((self.waypoints[closest - 1][0], self.waypoints[closest - 1][1]))
             else:
-                front = np.array((waypoints[closest+1][0], waypoints[closest+1][1]))
-                back = np.array((waypoints[closest][0], waypoints[closest][1]))
+                if dist(current_location, self.waypoints[closest - 1]) < dist(current_location,
+                                                                              self.waypoints[closest + 1]):
+                    front = np.array((self.waypoints[closest][0], self.waypoints[closest][1]))
+                    back = np.array((self.waypoints[closest - 1][0], self.waypoints[closest - 1][1]))
+                else:
+                    front = np.array((self.waypoints[closest + 1][0], self.waypoints[closest + 1][1]))
+                    back = np.array((self.waypoints[closest][0], self.waypoints[closest][1]))
 
-        p3 = np.array((current_location[0], current_location[1]))
-        n = back - front
-        v = p3 - front
-        p4 = front + n * (np.dot(v, n) / np.dot(n, n))
-        d = dist(p4, p3)
+            p3 = np.array((current_location[0], current_location[1]))
+            n = back - front
+            v = p3 - front
+            p4 = front + n * (np.dot(v, n) / np.dot(n, n))
+            d = dist(p4, p3)
 
-        if max_distance is None:
-            max_distance = d
-        elif d > max_distance:
-            max_distance = d
+            # TODO: Figure out a way to differentiate between which side of the waypoint the lad is on
 
-        angle = np.arctan2((front[1] - back[1]), (front[0] - back[0]))
+            self.lateral_error += [
+                (vehicle_point.header.stamp.sec + (0.000000001 * vehicle_point.header.stamp.nanosec), d)]
 
-        direction = vehicle_point.pose.pose.orientation
+            angle = np.arctan2((front[1] - back[1]), (front[0] - back[0]))
 
-        _, _, yaw = quaternion_to_euler(direction.x, direction.y, direction.z, direction.w)
+            direction = vehicle_point.pose.pose.orientation
 
-        heading_error = angle - yaw
-        while heading_error > np.pi:
-            heading_error = heading_error - (2 * np.pi)
-        while heading_error < -np.pi:
-            heading_error = heading_error + (2 * np.pi)
+            _, _, yaw = quaternion_to_euler(direction.x, direction.y, direction.z, direction.w)
 
+            heading_error = angle - yaw
+            while heading_error > np.pi:
+                heading_error = heading_error - (2 * np.pi)
+            while heading_error < -np.pi:
+                heading_error = heading_error + (2 * np.pi)
+            self.lateral_error += [
+                (vehicle_point.header.stamp.sec + (0.000000001 * vehicle_point.header.stamp.nanosec), heading_error)]
 
-        if max_heading_error is None:
-            logga.info('New max Heading Error of ' + str(heading_error) + 'at the point of ' + str(current_location))
-            logga.info('Back point: ' + str(back) + ', front point: ' + str(front) + ', Yaw' + str(yaw))
-            max_heading_error = abs(heading_error)
-        elif abs(heading_error) > max_heading_error:
-            logga.info('New max Heading Error of ' + str(heading_error) + 'at the point of ' + str(current_location))
-            logga.info('Back point: ' + str(back) + ', front point: ' + str(front) + ', Yaw' + str(yaw))
-            max_heading_error = abs(heading_error)
-    return max_heading_error, max_distance
+    def print_measurement_diagrams(self, vehicle_path, waypoints):
+        plt.plot(list(zip(*self.lateral_error))[0], list(zip(*self.lateral_error))[1])
+        plt.show()
+
+        plt.plot(list(zip(*self.heading_error))[0], list(zip(*self.heading_error))[1])
+        plt.show()
+
+    def print_path_diagram(self):
+        vehicle_path_points = []
+        for p in self.vehicle_path:
+            vehicle_path_points += [(p.pose.pose.position.x, p.pose.pose.position.y)]
+        plt.plot(list(zip(*self.waypoints))[0], list(zip(*self.waypoints))[1])
+        plt.plot(list(zip(*vehicle_path_points))[0], list(zip(*vehicle_path_points))[1])
+        plt.show()
+
+    def get_maximum_errors(self):
+        # Get the closest two points to the cars position
+        heading_errors = list(zip(*self.heading_error))[1]
+
+        lateral_errors = list(zip(*self.lateral_error))[1]
+
+        max_distance = max(lateral_errors)
+
+        max_heading = max(heading_errors)
+        min_heading = min(heading_errors)
+        max_heading_error = max_heading if (abs(min_heading) < max_heading) else abs(min_heading)
+
+        return max_heading_error, max_distance
 
 
 class orient():
