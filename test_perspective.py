b08af6192c509bc5be468a1b75c13a28526f0abd
ianh6 <ian.a.holmes@sympatico.ca>
2024-03-16 16:18:33 -0400

    upload test lines

--- test_perspective.py
+++ test_perspective.py
@@ -11,8 +11,6 @@ import lane
 import sys
 from PIL import Image
 
-ORIGINAL_SIZE = 695, 1263
-UNWARPED_SIZE = ORIGINAL_SIZE
 
 
 # example_images = ['./test_images/straight_lines1.jpg', './test_images/straight_lines2.jpg']
@@ -26,8 +24,8 @@ UNWARPED_SIZE = ORIGINAL_SIZE
 # cv2.fillPoly(roi, [roi_points], (255,0,0))
 
 
-# img = mpimg.imread('ikea.png')
-img = np.asarray(Image.open('truckcrash.jpg')).copy()
+img = mpimg.imread('ikea.png')
+# img = np.asarray(Image.open('truckcrash.jpg')).copy()
 ORIGINAL_SIZE = img.shape[0], img.shape[1]
 UNWARPED_SIZE = ORIGINAL_SIZE
 
@@ -35,15 +33,13 @@ plt.imshow(img)
 
 print('img: ', img.shape)
 
-low_thresh = 100
-high_thresh = 200
-
 Lhs = np.zeros((2, 2), dtype = np.float32)
 Rhs = np.zeros((2, 1), dtype = np.float32)
 x_max = 0
 x_min = 2555
 
 lanes = lane.lanes
+
 
 # print('lanes[0]: ', lanes[0])
 for line in lanes:
@@ -71,8 +67,8 @@ for line in lanes:
         Lhs += outer
         Rhs += np.matmul(outer, pt) #use matmul for matrix multiply and not dot product
 
-        cv2.line(np.int32(img), (x1, y1), (x2, y2), (255, 0, 0), thickness = 1)
-        
+        print(x1, y1)
+        cv2.line(np.int32(img), (x1, y1), (x2, y2), (255, 0, 0), thickness = 10)
         x_iter_max = max(x1, x2)
         x_iter_min = min(x1, x2)
         x_max = max(x_max, x_iter_max)
@@ -119,8 +115,8 @@ p2 = [vp[0] + width/2, top]
 p3 = find_pt_inline(p2, vp, bottom)
 p4 = find_pt_inline(p1, vp, bottom)
 
-print('**********************\n{}\n{}\n{}\n{}\n**********************'.format(p1, p2, p3, p4))
-print(len(p1), len(p2), len(p3), len(p4))
+# print('**********************\n{}\n{}\n{}\n{}\n**********************'.format(p1, p2, p3, p4))
+# print(len(p1), len(p2), len(p3), len(p4))
 
 src_pts = np.array([p1, p2, p3, p4], dtype=object)
 
@@ -131,7 +127,7 @@ dst_pts = np.float32([[0, 0], [UNWARPED_SIZE[0], 0],
                         [UNWARPED_SIZE[0], UNWARPED_SIZE[1]],
                         [0, UNWARPED_SIZE[1]]])
 
-# cv2.polylines(img, [src_pts.astype(np.int32)],True, (0,200,100), thickness=5)
+cv2.polylines(img, [src_pts.astype(np.int32)],True, (255,0,0), thickness=5)
 plt.plot(p1[0], p1[1], 'r+')
 plt.plot(p2[0], p2[1], 'r+')
 plt.plot(p3[0], p3[1], 'r^')
@@ -148,7 +144,7 @@ print('M.shape=',M.shape)
 print('M',M)
 warped = cv2.warpPerspective(img, M, UNWARPED_SIZE)
 print('warped.shape=',warped.shape)
-print('warped: ', warped)
+# print('warped: ', warped)
 
 
 # this is very gross and I don't like it either, but it works for now, the formatting of the input is difficult :(
@@ -158,12 +154,25 @@ for line in lanes:
     t_points = []
     for point in points:
         t_point = cv2.perspectiveTransform(np.float32([[[point[0],point[1]]]]), M)
-        print(t_point)
+        # print(t_point)
+        # t_point[0][0][0] = t_point[0][0][0]  - ORIGINAL_SIZE[0] / 2
+        # t_point[0][0][1] = abs(t_point[0][0][1]  - ORIGINAL_SIZE[1])
         t_points.append(t_point[0][0])
     lines.append(t_points)
 
-print(t_points)
+# print(t_points)
 plt.clf()
 for line in lines:
-    cv2.polylines(warped, [np.array(line).astype(np.int32)], False, (255,0,0), thickness=10)
+    cv2.polylines(warped, [np.array(line).astype(np.int32)], False, (255,0,0), thickness=5)
 plt.imshow(warped)
+
+x_points = []
+y_points = []
+plt.clf()
+for line in lines:
+    for point in line:
+        x_points.append(point[0])
+        y_points.append(point[1])
+    plt.plot(x_points, y_points)
+    x_points = []
+    y_points = []
