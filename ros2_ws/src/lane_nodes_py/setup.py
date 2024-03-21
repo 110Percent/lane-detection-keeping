8c6d25c1ea9b334a01c8c67eff56f81137d677f6
Robert Simionescu <robsim378@gmail.com>
2024-03-21 01:25:18 -0400

    Updated launch file to include confidence subsystem

--- ros2_ws/src/lane_nodes_py/setup.py
+++ ros2_ws/src/lane_nodes_py/setup.py
@@ -24,7 +24,8 @@ setup(
             'keeping_node = lane_nodes_py.keeping_node:main',
             'detection_node = lane_nodes_py.detection_node:main',
             'camera_controller_node = lane_nodes_py.camera_controller_node:main',
-            'evaluation_keeping_node = lane_nodes_py.evaluation_keeping_node:main'
+            'evaluation_keeping_node = lane_nodes_py.evaluation_keeping_node:main',
+            'confidence_node = lane_nodes_py.confidence_node:main'
         ],
     },
 )
